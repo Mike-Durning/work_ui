bf51e44abd1dd655ca18c630f0996ca145650c7c
Michael Durning <117465192+Mike-Durning@users.noreply.github.com>
2023-11-03 11:32:29 -0400

    Add files via upload

---
+++ pyqt_proj/src/excel_macro.py
@@ -0,0 +1,82 @@
+import time
+import pyautogui
+import pygetwindow as gw
+
+def hotkey_single(keys, delay=0.05):
+    for key in keys:
+        pyautogui.press(key)
+        #print("Key", key, "Pressed!")
+        time.sleep(delay)
+    
+def hotkey_double(keys, delay=0.05):
+    pyautogui.hotkey(*keys)
+    #print("Hotkey", keys, "Pressed!")
+    time.sleep(delay)
+
+def excel_macro(search_list_format_info):
+    try:        
+        todays_date = search_list_format_info["date"]
+        search_list = search_list_format_info["format"]
+        client_name = search_list_format_info["client_name"]
+        
+        filename_save = filename_save = client_name + " " + search_list + " " + todays_date  # noqa: E501
+        
+        time.sleep(0.15)
+        
+        target_window = gw.getWindowsWithTitle(filename_save)
+
+        # Check if the window was found
+        if target_window:
+            # Bring the window to the foreground
+            target_window[0].activate()
+        else:
+            print(f"Window with title '{filename_save}' not found.")
+        
+        # Select Total Column
+        hotkey_single(['alt', 'j', 't', 't'])
+
+        # Manuever to Total Row
+        hotkey_double(['ctrl', 'down'])
+        hotkey_single(['down'])
+        
+        pyautogui.typewrite("Total")
+        
+        hotkey_single(['right'])
+        
+        # Enter total function
+        pyautogui.typewrite("=SUBTOTAL(103,[Patient Name])")
+        hotkey_single(['enter'])
+        hotkey_single(['up'])
+        
+        # Select total row
+        hotkey_double(['shift', 'space'])
+        
+        # center total row
+        hotkey_single(['alt', 'h', 'a', 'c'])
+        
+        # fill total row
+        hotkey_single(['alt', 'h', 'h'])
+
+        # Select Correct Fill Color
+        for _ in range(3):
+            pyautogui.press('down')
+        pyautogui.press('enter')
+        
+        hotkey_single(['left'])
+        
+        hotkey_double(['ctrl', 'up'])
+
+        # Select entire table
+        hotkey_double(['ctrl', 'a'])
+        
+        hotkey_double(['ctrl', 'a'])
+
+        # Select Autoresize
+        hotkey_single(['alt', 'h', 'o', 'i'])
+        
+        hotkey_single(['left'])
+        
+        hotkey_double(['ctrl', 's'])
+                
+    except Exception as e:
+        print(f"An error occurred: {e}")
---
+++ pyqt_proj/src/formatter_ui.py
@@ -0,0 +1,298 @@
+from PyQt6.QtWidgets import QTabWidget, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QLineEdit  # noqa: E501
+from PyQt6.QtWidgets import QApplication, QComboBox, QPushButton, QCheckBox, QTextEdit, QHBoxLayout  # noqa: E501
+import sys
+from datetime import datetime
+from startup_file_manage import FileManager
+from excel_macro import excel_macro
+from excel_manipulation import ExcelManipulator
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+                
+        self.setWindowTitle("RAI Formatter")
+        self.setGeometry(175, 25, 1000, 700)
+        
+        main_widget = QWidget()
+        self.setCentralWidget(main_widget)
+
+        # Layouts
+        main_layout = QVBoxLayout() # Changed to a vertical layout for the entire window
+        tab_widget = QTabWidget()
+        main_layout.addWidget(tab_widget) # Add the tab widget to the main layout
+
+        # Create the first tab
+        first_tab = QWidget()
+        tab_widget.addTab(first_tab, "Action Center") # Add the first tab to the tab widget  # noqa: E501
+
+        button_layout = QGridLayout() # Grid layout for buttons
+        
+        # Create a QTextEdit widget for the first output
+        self.output_text1 = QTextEdit()
+        self.output_text1.setReadOnly(True)
+
+        # Create a QTextEdit widget for the second output
+        self.output_text2 = QTextEdit()
+        self.output_text2.setReadOnly(True)
+
+        # Add QTextEdit widgets directly to the layout
+        output_layout = QHBoxLayout()
+        output_layout.addWidget(self.output_text1, 2)  # 2/3 of horizontal space
+        output_layout.addWidget(self.output_text2, 1)  # 1/3 of horizontal space
+
+        # Add layouts and widgets to the first tab
+        first_tab_layout = QVBoxLayout()
+        first_tab_layout.addLayout(button_layout)
+        first_tab_layout.addLayout(output_layout)  # Add the horizontal output layout
+        first_tab.setLayout(first_tab_layout)  # Set the layout for the first tab
+
+        # Create a container widget for the layouts
+        container_widget = QWidget()
+        container_widget.setLayout(main_layout)
+        self.setCentralWidget(container_widget)
+
+        # Apply dark mode stylesheet
+        self.setStyleSheet("""
+            QMainWindow {
+                background-color: #333;
+                color: #FFF;
+            }
+                  
+            QPushButton, QRadioButton, QCheckBox {
+                background-color: #444;
+                color: #FFF;
+                text-align: center;
+                border: 1px solid #555;
+                padding: 5px;
+            }
+            
+            QComboBox, QComboBox QAbstractItemView::item {
+                background-color: #444;
+                text-align: center;
+                color: #FFF;
+            }
+            
+            QFrame {
+                background-color: #444;
+                border: 1px solid #555;
+            }
+            QLineEdit {
+                background-color: #555;
+                color: #FFF;
+                border: 1px solid #666;
+                padding: 5px;
+            }
+            QTextEdit {
+                background-color: #555;
+                color: #FFF;
+                border: 1px solid #666;
+                padding: 5px;
+            }
+        """)
+   
+        dropdown_position = {
+            # First button, dropdown menu
+            "Select Client"      : (0, 0)
+        }
+        
+        add_remove_client_position = {
+            # Middle Left Button Row - ADD REMOVE CLIENT
+            "Add Client"         : (1, 0),
+            "Remove Client"      : (1, 1),
+        }
+        
+        button_positions = {
+            
+            # Top Button Row | Excluding first
+            "Excel Manipulation" : (0, 1),
+            "Excel Macro"        : (0, 2),
+           
+            
+            # Middle Right Button Row
+            "Clear Output"       : (1, 2),
+            
+            # Bottom Toggle Row
+            "Toggle Short Search"    : (2, 0),
+            "Toggle Department"      : (2, 1),
+            "Toggle Custom Directory": (2, 2)
+        }
+
+        for text, (row, col) in add_remove_client_position.items():
+            self.line_edit = QLineEdit()
+            button_layout.addWidget(self.line_edit, row, col)
+            if text == "Add Client":
+                pass
+            if text == "Remove Client":
+                pass
+                    
+        for text, (row, col) in dropdown_position.items():
+            self.drop_menu = QComboBox()
+            button_layout.addWidget(self.drop_menu, row, col)
+            if text == "Select Client":
+                    self.drop_menu.activated.connect(self.on_drop_menu_execute)
+
+        for text, (row, col) in button_positions.items():
+            if text.startswith("Toggle"):
+                button = QPushButton(text)
+                toggle_button = QCheckBox(text)
+                button_layout.addWidget(button, row, col)
+                button_layout.addWidget(toggle_button, row, col)
+                
+                if text == "Toggle Short Search":
+                    toggle_button.stateChanged.connect(self.is_lhi)
+                elif text == "Toggle Department":
+                    toggle_button.stateChanged.connect(self.has_department)
+                elif text == "Toggle Custom Directory":
+                    toggle_button.stateChanged.connect(self.is_default_path)
+            else:
+                button = QPushButton(text)
+                button_layout.addWidget(button, row, col)
+                
+                if text == "Excel Manipulation":
+                    button.clicked.connect(self.excel_manip)
+                elif text == "Excel Macro":
+                    button.clicked.connect(self.on_excel_macro)
+                elif text == "Clear Output":
+                    button.clicked.connect(self.clear_output)
+
+        
+        self.file_manager = FileManager()
+        self.excel_manipulator = ExcelManipulator()
+        
+        todays_date = datetime.now().strftime('%m.%d.%Y')
+
+        self.search_list_format_info = {
+                            "client_name": "NO CLIENT SELECTED",
+                            "format"     : "Search List",
+                            "date"       : todays_date,
+                            "file_type"  : ".xlsx",
+                            "custom_dir" : None
+        }
+
+        self.toggle_states = {
+            "Toggle LHI Search List"  : False,
+            "Toggle Department"       : False,
+            "Toggle Custom Directory" : False
+        }
+
+        self.file_manager.save_toggle_states_to_json(self.toggle_states)
+        self.file_manager.save_client_info_to_json(self.search_list_format_info)
+        self.populate_combo_box()
+
+    def populate_combo_box(self):
+        self.file_manager.create_folders()
+        self.file_manager.create_json_files()
+        self.output_text2.clear()
+        self.output_text2.append(self.file_manager.print_clients_json()[0])
+              
+        self.client_list = self.file_manager.print_clients_json()[1]
+        self.client_list = self.client_list.values() 
+        self.drop_menu.addItems(self.client_list)
+    
+    def clear_repop(self):
+        # Clear the existing items in the QComboBox
+        self.output_text2.clear()
+    
+        value = self.drop_menu.currentText()
+        
+        if not value:
+            self.output_text1.append("No client selected in the dropdown.")
+            return
+        
+        self.output_text1.append(f"Selected Client: {value}")
+        
+        selected_client = self.file_manager.select_client_by_value(value)
+        
+        if selected_client is not None:
+            print(f"\nSelected Client: {selected_client}\n") 
+            self.search_list_format_info["client_name"] = selected_client
+            
+            for key, value in self.search_list_format_info.items():
+                self.output_text2.append(f"{key}: {value}")  
+            
+            self.output_text2.append("")
+            
+            for key, value in self.toggle_states.items():
+                self.output_text2.append(f"{key}: {value}")  
+                
+        else:
+            print("\nInvalid option.\n")
+    
+    def on_drop_menu_execute(self):
+        self.clear_repop()               
+        
+    def excel_manip(self):
+        self.output_text1.append("Excel Manipulation Selected")
+        search_list_format_info = self.search_list_format_info
+        
+        if search_list_format_info.get("client_name") == "NO CLIENT SELECTED":
+            self.output_text1.append("No Client Selected")
+        
+        else:
+            concat_excel_file = self.excel_manipulator.pandas_column_rearrange()
+            wb = self.excel_manipulator.openpyxl_format_workbook(concat_excel_file)
+            self.file_manager.save_xlsx(wb, self.search_list_format_info)
+
+    def on_excel_macro(self):
+        self.output_text1.append("Excel Macro Selected")
+        search_list_format_info = self.search_list_format_info
+        
+        if search_list_format_info.get("client_name") == "NO CLIENT SELECTED":
+            self.output_text1.append("No Client Selected")
+        
+        else:
+            excel_macro(search_list_format_info)
+
+    def client_add(self):
+        self.output_text1.append("client added")  
+
+    def client_del(self):
+        self.output_text1.append("client removed")
+
+
+    def clear_output(self):
+            self.output_text1.clear()
+            self.populate_combo_box()
+    # Define functions for toggle buttons
+    def is_lhi(self):
+        if self.sender().isChecked():
+            self.output_text1.append("'Toggle Shortened Search List' is ON")
+            self.toggle_states["Toggle Shortened Search List"] = True
+
+        else:
+            self.output_text1.append("'Toggle Shortened Search List' is OFF")
+            self.toggle_states["Toggle Shortened Search List"] = False
+        self.file_manager.save_toggle_states_to_json(self.toggle_states)
+        self.clear_repop()
+
+    def has_department(self):
+        if self.sender().isChecked():
+            self.output_text1.append("'Toggle Department' is ON")
+            self.toggle_states["Toggle Department"] = True
+
+        else:
+            self.output_text1.append("'Toggle Department' is OFF")
+            self.toggle_states["Toggle Department"] = False
+        self.file_manager.save_toggle_states_to_json(self.toggle_states)
+        self.clear_repop()
+
+    def is_default_path(self):
+        if self.sender().isChecked():
+            self.output_text1.append("'Toggle Custom Directory is ON")
+            custom_dir = self.file_manager.return_path()
+            self.search_list_format_info["custom_dir"] = custom_dir
+            self.toggle_states["Toggle Custom Directory"] = True
+            
+
+        else:
+            self.output_text1.append("'Toggle Custom Directory is OFF")
+            self.toggle_states["Toggle Custom Directory"] = False
+        self.file_manager.save_toggle_states_to_json(self.toggle_states)
+        self.clear_repop()
+            
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec())
